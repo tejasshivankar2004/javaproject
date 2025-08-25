09f6f1aac1186b668e6210e1f9b257cc00350cb1
Tejas Shivankar <tejasshivankar2004@gmail.com>
2025-08-25 19:25:41 +0530

    variable Clomiit

--- src/main/java/variable.java
+++ src/main/java/variable.java
@@ -1,2 +1,33 @@
-public class variable {
+
+
+   class Variable{
+    public static void main(String[] args) {
+
+        int age = 21;
+        String name = "TEJAS";
+        double height = 5.9;
+        boolean isStudent = true;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is a student? " + isStudent);
+    }
 }
+
+
+//
+//class Variable {
+//    public static void main(String[] args) {
+//        int x = 5;
+//        int roll_number$Student = 101;
+//        int _x = 20;
+//        int myString = 10;  // Avoid using 'string' as a variable name
+//
+//        System.out.println("x: " + x);
+//        System.out.println("roll_number$Student: " + roll_number$Student);
+//        System.out.println("_x: " + _x);
+//        System.out.println("myString: " + myString);
+//    }
+//}
